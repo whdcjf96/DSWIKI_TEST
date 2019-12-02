d937e11dc3d83aff4be815777af12e75f914ef2a
Yu Jong Chuel <aaaaaaa123455549@gmail.com>
2019-12-02 21:31:40 +0900

    에디트하다맘

--- project.js
+++ project.js
@@ -11,7 +11,11 @@ app.use(express.static('public'));
 app.set('view engine','ejs');
 app.set('views',__dirname+'/views');
 
-var database = []
+var human_database = []
+var study_database = []
+var play_database = []
+var restaurant_database = []
+var circle_database = []
 
 var search_database = []
 
@@ -29,9 +33,9 @@ app.all('/result',
 	{
 	
 		var a = request.body.text;
-		if(a in database){
-			response.render('search_success', {'text': request.body.text,'introduce':database.introduce ,
-												'picture':database.picture,'talk':database.talk});
+		if(a in human_database){
+			response.render('search_success', {'text': request.body.text,'introduce':human_database.introduce ,
+												'picture':human_database.picture,'talk':human_database.talk});
 		}else{														//데이터베이스 연동 값 가져오는 부분
 			response.render('search_fail', {'text': request.body.text});
 		}
@@ -40,62 +44,65 @@ app.all('/result',
 
 app.all('/study_insert_to_success', function(request,response){
 	search_database.push({text:request.body.text})
-	database.push({explain:request.body.explain_study})
-	database.push({explain:request.body.tip_study})
-	console.log(database)
+	study_database.push({explain:request.body.explain_study})
+	study_database.push({explain:request.body.tip_study})
+	console.log(study_database)
 	response.render('study_success',{'text':request.body.text, 'explain':request.body.explain_study ,
 	'tip':request.body.tip_study});
 })
 app.all('/human_insert_to_success', function(request,response){
 	search_database.push({text:request.body.text})
-	database.push({introduce:request.body.introduce})
-	database.push({character:request.body.character})
-	database.push({ssul:request.body.ssul})
-	console.log(database)
+	human_database.push({introduce:request.body.introduce})
+	human_database.push({character:request.body.character})
+	human_database.push({ssul:request.body.ssul})
+	console.log(human_database)
 	response.render('human_success',{'text':request.body.text, 'introduce':request.body.introduce ,
 	'character':request.body.character, 'ssul': request.body.ssul});
 })
 app.all('/play_insert_to_success', function(request,response){
 	search_database.push({text:request.body.text})
-	database.push({explain:request.body.explain_study})
-	database.push({explain:request.body.tip_study})
-	console.log(database)
-	response.render('study_success',{'text':request.body.text, 'explain':request.body.explain_study ,
-	'tip':request.body.tip_study});
+	play_database.push({sort:request.body.explain_sort})
+	play_database.push({explain:request.body.explain_play})
+	play_database.push({tip:request.body.tip_play})
+	console.log(play_database)
+	response.render('play_success',{'text':request.body.text, 'sort':request.body.sort, 'explain':request.body.explain_play ,
+	'tip':request.body.tip_play});
 })
 app.all('/restaurant_insert_to_success', function(request,response){
 	search_database.push({text:request.body.text})
-	database.push({explain:request.body.explain_study})
-	database.push({explain:request.body.tip_study})
-	console.log(database)
-	response.render('study_success',{'text':request.body.text, 'explain':request.body.explain_study ,
-	'tip':request.body.tip_study});
+	restaurant_database.push({overview_restaurant:request.body.overview_restaurant})
+	restaurant_database.push({menu:request.body.menu})
+	restaurant_database.push({tip:request.body.tip_restaurant})
+	console.log(restaurant_database)
+	response.render('restaurant_success',{'text':request.body.text, 'overview_restaurant':request.body.overview_restaurant, 'menu':request.body.menu ,
+	'tip':request.body.tip_restaurant});
 })
 app.all('/circle_insert_to_success', function(request,response){
 	search_database.push({text:request.body.text})
-	database.push({explain:request.body.explain_study})
-	database.push({explain:request.body.tip_study})
-	console.log(database)
-	response.render('study_success',{'text':request.body.text, 'explain':request.body.explain_study ,
+	circle_database.push({overview_circle:request.body.overview})
+	circle_database.push({activity:request.body.activity})
+	circle_database.push({repute:request.body.repute})
+	circle_database.push({tip:request.body.tip_circle})
+	console.log(circle_database)
+	response.render('circle_success',{'text':request.body.text, 'overview_circle':requset.body.overview_circle, 'activity':requset.body.activity, 'repute':request.body.repute ,
 	'tip':request.body.tip_study});
 })
 
-app.all('/edit',		//사진은 어떻게할지 정해야함
+app.all('/human_edit',		
 	function(request,response){
-		var human = ["개미분식은 치제뚝이 유명하다 돈까스도 맛잇다.","사진의 위치","밑반찬으로주는 떡볶이도 맛잇다","개미분식"]
-		var key= request.body.key
-		if(key == '0'){
-			response.render('search_success',{'text': request.body.text, 'introduce':request.body.intro_in,
-			'picture':human[1],'talk':human[2]})
-			human[0] = request.body.intro_in;
-			console.log(human[0]);
-		}else if(key == '2'){
-			response.render('search_success',{'text': request.body.text, 'introduce':human[0],
-			'picture':human[1],'talk':request.body.talk_in})
-			human[2] = request.body.talk_in;
-			console.log(human[0]);
-			console.log(human[2]);
+		var key= request.body.key;
+		switch(key){
+				case '1': human_database.introduce=request.body.introduce_in; break;
+				case '2': human_database.character=request.body.character_in; break;
+				case '3': human_database.ssul=request.body.ssul_in; break;
+
 		}
+			
+		console.log(key)
+		console.log(human_database)
+		response.render('human_success',{'text':request.body.text, 'introduce':human_database[0][0],'character':human_database.character,'ssul':human_database.ssul})
+			
+		
 
 		
 	})
